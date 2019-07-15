4a64215fbc07459ffacf48a37f4845a8d20197ec
mountolive <lpguercio@gmail.com>
2019-07-15 19:07:37 -0400

    +

--- app_server/services/base.js
+++ app_server/services/base.js
@@ -9,6 +9,7 @@ class BaseService {
       if(err) error(res, err);
       else if([200, 201, 204].includes(res.statusCode)) success(body);
       else if(res.statusCode === 404) {
+        console.log("COÑO DE LA MADRE ESTOY AQUI");
         error(res, {code: 404, errorMessage: 'No documents found'});
       }
     });
