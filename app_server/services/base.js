db46be2dec550eee479bed29b7086c4ed153e357
mountolive <lpguercio@gmail.com>
2019-07-15 17:00:06 -0400

    +

--- app_server/services/base.js
+++ app_server/services/base.js
@@ -8,8 +8,8 @@ class BaseService {
     request(reqOptions, (err, res, body) => {
       if(err) error(res, err);
       else if([200, 201, 204].includes(res.statusCode)) success(body);
-      else if(res.statusCod === 404) {
-        error(res, new Error('Document(s) missing'));
+      else if(res.statusCode === 404) {
+        error(res, {status: 404, stack: 'No documents found'});
       }
     });
   }
