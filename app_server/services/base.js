480c4391882c9c1484e5a6b75827d388bc2f590f
mountolive <lpguercio@gmail.com>
2019-07-15 19:01:41 -0400

    +

--- app_server/services/base.js
+++ app_server/services/base.js
@@ -9,7 +9,7 @@ class BaseService {
       if(err) error(res, err);
       else if([200, 201, 204].includes(res.statusCode)) success(body);
       else if(res.statusCode === 404) {
-        error(res, {status: 404, stack: 'No documents found'});
+        error(res, {code: 404, errorMessage: 'No documents found'});
       }
     });
   }
